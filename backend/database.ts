e4fe35d09645733fec3b6cca41f10caef40abbe7
matteDotta <m.dotta.2662@vallauri.edu>
2025-04-11 10:52:32 +0000

    fix: ho diviso il server e ho creato l'endpoint per creare il token

---
+++ backend/database.ts
@@ -0,0 +1,28 @@
+// database.ts
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+
+// Configurazione connessione al DB
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(process.env.PATH_TO_CA),
+  },
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Errore connessione al database:', err);
+    process.exit(1);
+  }
+  console.log('Connesso al database MySQL');
+});
+
+export { db }; // Esportiamo la connessione
