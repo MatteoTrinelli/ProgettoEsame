fe05675dc11d208e57284c1811cc64f07b3ccd16
Matteo <132984572+MatteoTrinelli@users.noreply.github.com>
2025-04-12 10:02:29 +0000

    creazione di Profilo,Palestra,impostazioni a mettere nella dashboard

---
+++ frontend/src/app/components/palestra/palestra.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palestra',
+  imports: [],
+  templateUrl: './palestra.component.html',
+  styleUrl: './palestra.component.css'
+})
+export class PalestraComponent {
+
+}
--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ProfiloComponent } from '../../components/profilo/profilo.component';
+import { PalestraComponent } from '../../components/palestra/palestra.component';
+import { ImpostazioniComponent } from '../../components/impostazioni/impostazioni.component';
 @Component({
   selector: 'app-dashboard',
-  imports: [ProfiloComponent],
+  imports: [ProfiloComponent, PalestraComponent, ImpostazioniComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
