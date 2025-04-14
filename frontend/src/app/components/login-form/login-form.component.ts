ddda1d451e9abc8f5d4759e6f0bb79e6c7ce2dfa
matteDotta <m.dotta.2662@vallauri.edu>
2025-04-14 07:24:48 +0000

    fix: messo tema rosso/nero/bianco al login

--- frontend/src/app/components/login-form/login-form.component.ts
+++ frontend/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
@@ -6,41 +5,40 @@ import { UserService } from '../../services/auth/user.service';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ReactiveFormsModule,RouterLink],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
-
 export class LoginFormComponent {
   currentYear: number = new Date().getFullYear();
 
   constructor(
-    private userService:UserService,
-    private Router:Router,
-  ){  }
+    private userService: UserService,
+    private router: Router,
+  ) {}
 
- loginForm = new FormGroup({
+  loginForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
-  async onSubmit() : Promise<void> {
-    if (!this.loginForm.valid) {
-      return;
-    }
-  try {
+
+  async onSubmit(): Promise<void> {
+    if (!this.loginForm.valid) return;
+    try {
       const loginOk = await this.userService.login(
         this.loginForm.value.username!,
         this.loginForm.value.password!,
       );
       if (loginOk) {
-        this.Router.navigate(["dashboard"]);
-      }
-      else{
+        this.router.navigate(["dashboard"]);
+      } else {
         alert("Incorrect Username or password");
       }
     } catch (err) {
       const error = err as Error;
-    } 
-  }  
+      console.error(error.message);
+    }
+  }
 }
