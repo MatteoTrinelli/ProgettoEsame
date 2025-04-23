14dd9cb498f4d6ad031df4825b93135deef80ca7
Matteo <132984572+MatteoTrinelli@users.noreply.github.com>
2025-04-23 07:28:59 +0000

    creato la pagina di registrazione maestro

---
+++ frontend/src/app/components/registrazione-maestro/registrazione-maestro.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrazioneMaestroComponent } from './registrazione-maestro.component';
+
+describe('RegistrazioneMaestroComponent', () => {
+  let component: RegistrazioneMaestroComponent;
+  let fixture: ComponentFixture<RegistrazioneMaestroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegistrazioneMaestroComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegistrazioneMaestroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/registrazione-maestro/registrazione-maestro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrazione-maestro',
+  imports: [],
+  templateUrl: './registrazione-maestro.component.html',
+  styleUrl: './registrazione-maestro.component.css'
+})
+export class RegistrazioneMaestroComponent {
+
+}
