34ba99665dbb8473238e684f1c5ea3428d663c20
Matteo <132984572+MatteoTrinelli@users.noreply.github.com>
2025-04-30 06:48:12 +0000

    prova per la registrazione

--- frontend/src/app/components/registrazione-allievo/registrazione-allievo.component.ts
+++ frontend/src/app/components/registrazione-allievo/registrazione-allievo.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrazione-allievo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registrazione-allievo.component.html',
   styleUrl: './registrazione-allievo.component.css'
 })
 export class RegistrazioneAllievoComponent {
 
+
+  registerForm = new FormGroup({
+    nome: new FormControl('abc',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    cognome: new FormControl('abc',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    dob: new FormControl('',[Validators.required]),
+    nazionalità: new FormControl('abc',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    luogoNascita: new FormControl('abc',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    disciplinaPrinciplae : new FormControl('abc',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    gradoDisciplinaPrincipale : new FormControl('abc',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]), 
+  });
 }
